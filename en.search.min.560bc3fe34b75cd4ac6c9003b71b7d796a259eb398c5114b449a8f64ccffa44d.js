ada1404244b55c528b5e702f2e2e546758e80941
elsapollen <elsapollen@users.noreply.github.com>
2020-02-28 15:53:56 +0000

    Automated deployment: Fri Feb 28 15:53:56 UTC 2020 e89d785fd8ea5cbe4f293ad7c258fbc29700e06b

--- en.search.min.560bc3fe34b75cd4ac6c9003b71b7d796a259eb398c5114b449a8f64ccffa44d.js
+++ en.search.min.560bc3fe34b75cd4ac6c9003b71b7d796a259eb398c5114b449a8f64ccffa44d.js
@@ -2,7 +2,7 @@
 const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();e.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/reachy-docs/flexsearch.min.js');loadScript('/reachy-docs/en.search-data.min.4fe6859c6bd623c71597597f3164409407c89cb8d7b748d8a1f6bf53195d5eab.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/reachy-docs/flexsearch.min.js');loadScript('/reachy-docs/en.search-data.min.07746050adbb46f6b057bc5cf11b68082fa9c12cee955912fc7ef3cb09375c91.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
