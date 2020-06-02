b6b3038752a94c80af14f9f1bb90b46dd553575f
pierre-rouanet <pierre-rouanet@users.noreply.github.com>
2020-06-02 14:17:16 +0000

    Automated deployment: Tue Jun  2 14:17:16 UTC 2020 51198b98f0ec9ad4439691e80bcb8128d3f60da1

--- en.search.min.007d3371c44457c97498fc6410cf0cf00297ab2a8f1b29ac91647edf7f093a71.js
+++ en.search.min.007d3371c44457c97498fc6410cf0cf00297ab2a8f1b29ac91647edf7f093a71.js
@@ -2,7 +2,7 @@
 const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();e.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/reachy-docs/flexsearch.min.js');loadScript('/reachy-docs/en.search-data.min.a78e555d93061542bb09862ff147370a499f14aad9d70f8bd414b1279c770e53.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/reachy-docs/flexsearch.min.js');loadScript('/reachy-docs/en.search-data.min.538f8bd23ee63ab1a924a490c33242b1cf96fbb429544f349ea9d99f591be9b6.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
